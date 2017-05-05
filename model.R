88e90d19fdc03284421e8311a150fbb5ea4ee9fc
jarobar435 <jarobar435@student.polsl.pl>
2017-05-05 02:19:08 +0200

    Komentarze + usunięcie wektorów początkowych.

--- model.R
+++ model.R
@@ -1,48 +1,51 @@
-lekOrg = c(2.26,1.81,1.78,1.54,2.06,1.74,2.56,2.29,1.80,2.32,2.04,1.88,1.18,2.08,1.70,1.74,1.90,1.79,2.11,1.72,1.74,1.60,2.15,2.26,1.65,1.63,2.40,2.70,1.90,2.78,2.27,1.74,2.62,1.80,1.81,1.58,2.41,1.65,2.24,1.70,2.45,1.72,2.37,2.23,1.92,1.99,1.99,2.35,1.80,2.36,1.59,2.10,1.80) 
-sbpOrg = c(66,52,72,67,69,71,88,68,59,73,68,58,61,68,69,55,67,67,68,59,68,63,65,72,58,69,70,73,56,83,67,84,68,64,60,62,76,60,60,59,84,66,68,65,69,72,63,56,70,69,60,51,61)
-czasOrg = c(7,10,18,4,10,13,21,12,9,65,20,31,23,22,13,9,50,12,11,8,26,16,23,7,11,8,14,39,28,12,60,10,60,22,21,14,4,27,26,28,15,8,46,24,12,25,45,72,25,28,10,25,44)
+# Projekt: Metody statystyczne INF Sem. 4 Gr. 2
+# Temat: 3r - regresja wieloraka 
+# Autorzy: Jarosław Bartoszuk, Mateusz Boral, Szymon Mendla, Mateusz Nieć, Aleksander Patschek
 
-data = read.table("data.csv")
-lek = (data[,1])
-sbp = (data[,2])
-czas = (data[,3])
+data = read.table("data.csv") #Dane pomiarowe importujemy z odpowiednio sformatowanego pliku.
+lek = (data[,1])              #Pierwsza kolumna zawiera logarytm ilości leku w mg.
+sbp = (data[,2])              #Druga kolumna odpowiada średniemu poziomowi ciśnienia w hipotensji.
+czas = (data[,3])             #Trzecia odnosi się do czasu powrotu pacjenta do normy.
 
-
-X = cbind(1,lek,sbp)
+# Konstruowanie modelu
+X = cbind(1,lek,sbp)                  #Tworzymy macierz zmiennych objaśniających poprzez złączenie kolumn - jedynkowej oraz dwóch zmiennych niezależnych 
 Y = czas
-xtx = t(X)%*%X
-A = solve(xtx)%*%t(X)%*%Y
-model =A[1]+ A[2]*lek + A[3]*sbp
+xtx = t(X)%*%X                        #Funkcja t() tworzy macierz transponowaną; operator %*% służy do mnożenia macierzy.
+A = solve(xtx)%*%t(X)%*%Y             #Funcka solve() rozwiązuje układ równań powstały na skutek operacji.
+model = A[1] + A[2]*lek + A[3]*sbp    #Implementacja modelu regresjii wielorakiej (2.6.7.1 w podr.)
 
 
+# Weryfikacja modelu
+SSR = t(model-mean(Y))%*%(model-mean(Y))            #Sum of Square Regression - odchyleń wartości przewidywanych od średniej
+SSE = t((Y - model))%*%(Y-model)                    #Sum of Square Errors - reszt
+SST = t(Y-mean(Y))%*%(Y-mean(Y))                    #Sum of Square Total - odchyleń wartości obserwacji od średniej
+R2 = SSR/SST                                        #Współczynnik determinacji wielorakiej
+R2a = 1-((1-R2)*(NROW(model)-1)/(NROW(model)-2-1))  #Współczynnik dopasowania z uwzględnieniem ilości zmiennych i liczności prób.
 
-SSR = t(model-mean(Y))%*%(model-mean(Y))
-SSE = t((Y - model))%*%(Y-model)
-SST = t(Y-mean(Y))%*%(Y-mean(Y))
-R2 = SSR/SST
-R2a = 1-((1-R2)*(NROW(model)-1)/(NROW(model)-2-1))
 
-
-wariancja = SSE/(NROW(model)-2-1)
+wariancja = SSE/(NROW(model)-2-1)     #Funkcja NROW() zwraca ilość wierszy przekazanego obiektu.
 odchylenie = sqrt(wariancja)
 
-res = Y-model;
+# Analiza reszt:
+res = Y-model
 sum_res = sum(res)
 mean_res = mean(res)
 
-dplus= c();
-dminus= c();
-dys_nor = pnorm(sort(res))
+# Test Kołmogorowa-Smirnowa zgodnie ze wzorem ze sprawozdania
+dplus = c();
+dminus = c();
+dys_nor = pnorm(sort(res))                            #Funkcja pnorm() oblicza dystrybuantę.
 n = NROW(res)
-for(i in 1:n){
-  dplus = append(dplus,abs(i/n-dys_nor[i]))
+for(i in 1:n)
+{
+  dplus = append(dplus,abs(i/n-dys_nor[i]))           #Funkcja append() dopisuje wartość do wektora.
   dminus= append(dminus,abs(dys_nor[i]-(i-1)/n))
 }
 
 D = max(max(dplus),max(dminus))
 
-licznik =0;
-
+# Test Durbina-Watsona zgodnie ze wzorem ze sprawozdania
+licznik = 0;
 for (t in 2:NROW(model))
 {
   licznik = licznik+(res[t]-res[t-1])^2
@@ -50,10 +53,12 @@ for (t in 2:NROW(model))
 
 dw = licznik/sum(res^2)
 
+# Wypisywanie wyliczonych wartości:
 naglowek = c('A0', 'A lek', 'A sbp', 'SSR', 'SSE', 'SST', 'R2','R2a',  'sum res', 'avg res', 'wariancja', 'odchylenie', 'dw','D')
 wyniki = c(A[1], A[2], A[3], SSR, SSE, SST, R2, R2a,sum_res, mean_res, wariancja, odchylenie,dw,D )
 write.table(cbind(naglowek, wyniki),"",row.names = F)
 
+# Uzyskane wykresy:
 plot(model,res, col = ifelse(abs(res) >(3*odchylenie[1,1]),'red','green'))
 text(model,res, labels=1:NROW(res),cex= 0.7, pos=2)
 
